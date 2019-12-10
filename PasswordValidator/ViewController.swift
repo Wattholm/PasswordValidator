a0858bd1ecc5e81218e46c8ef922291b3be8cbd4
Kevin Joseph Mangulabnan <wattholm@users.noreply.github.com>
2019-12-10 13:04:59 +0800

    Initial Commit

---
+++ PasswordValidator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordValidator
+//
+//  Created by KJEM on 10/12/2019.
+//  Copyright © 2019 KJEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
